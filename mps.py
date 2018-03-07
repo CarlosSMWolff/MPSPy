ffdfde8bcaae10165de6f97bcf2953cdde7b2d72
CarlosSMWolff <carlossmwolff@gmail.com>
2018-03-07 13:00:09 +0900

    QHAN eliminado e integrado en mps.py

--- mps.py
+++ mps.py
@@ -10,7 +10,6 @@ from numpy import transpose, dot, reshape, sqrt,conjugate
 import time
 from mpmath import hyp3f2,fac,rf
 from scipy.linalg import expm
-from QHahn import *
 import scipy
 from scipy.sparse import csr_matrix, csc_matrix
 
@@ -753,6 +752,19 @@ def MPSTimeEvolutionCorrelationHT(lambdaVectorIni,gammaVectorIni,Hstat,HlocalTim
 				expH4List[siteLocalH-1] =  expm(-1j*dt*(1-2*thetaCons)*Hintsite[siteLocalH-1])
 
 	return [correlationGridTime,meanxGridTime,errorTime,gammaVector,lambdaVector]
+
+
+def QHahn(k,n,N):
+	return float(hyp3f2(-k,k+2,-n,2,-N,1))
+
+def rhon2(n,N):
+    return float(((-1)**n * rf(n+2,N+1)*rf(1,n)*fac(n))/((2*n+2)*fac(1+n)*rf(-N,n)*fac(N)))
+
+def An(n,N):
+    return ((n+2)**2*(N-n))/((2*n+2)*(2*n+3))
+    
+def Cn(n,N):
+    return (n*(n+N+2)*n)/((2*n+1)*(2*n+2))
 
 
 
